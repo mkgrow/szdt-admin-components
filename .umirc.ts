e577b87acd1f028be0080ba52f73392f576db523
makan <makan@sevencent.com>
2023-01-31 12:15:26 +0800

    update publicpath

--- .umirc.ts
+++ .umirc.ts
@@ -9,8 +9,7 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   base: '/',
-  publicPath: '/',
-  dynamicImport: {},
+  publicPath: './',
   navs: [
     {
       title: '组件',
