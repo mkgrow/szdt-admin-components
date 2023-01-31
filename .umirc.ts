4073fd10a55f412e1dcd771210a2136316927fef
makan <makan@sevencent.com>
2023-01-31 10:34:09 +0800

    update .umirc.ts and docs/index.md

--- .umirc.ts
+++ .umirc.ts
@@ -2,19 +2,16 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'szdt-admin-components',
+  hash: true,
   mode: 'site',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
-  base: process.env.NODE_ENV === 'production' ? '/szdt-admin-components/' : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/szdt-admin-components/' : '/',
+  base: '/',
+  publicPath: '/',
   dynamicImport: {},
   navs: [
-    {
-      title: '文档',
-      path: '/docs',
-    },
     {
       title: '组件',
       path: '/components',
@@ -24,4 +21,10 @@ export default defineConfig({
       path: 'https://4x.ant.design/components/overview-cn/',
     },
   ],
+  lessLoader: {
+    javascriptEnabled: true,
+  },
+  resolve: {
+    includes: ['docs', 'src'],
+  },
 });
--- docs/index.md
+++ docs/index.md
@@ -1,11 +1,14 @@
 ---
 nav:
-  path: /docs
+  path: /
   order: 0
 group:
-  title: 文档
-  path: '/docs'
+  path: '/'
   order: 0
+hero:
+  title: '思辙数据'
+  desc: '管理系统组件库'
+  actions:
+    - text: 开始使用
+      link: /components
 ---
-
-## 思辙数据组件库
