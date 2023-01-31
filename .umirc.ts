9a805c90de5678f26cb10029a1ebbbd6df5c5121
makan <makan@sevencent.com>
2023-01-31 11:50:19 +0800

    modify outputpath

--- .umirc.ts
+++ .umirc.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'docs',
+  outputPath: 'docs-dist',
   base: '/',
   publicPath: '/',
   dynamicImport: {},
