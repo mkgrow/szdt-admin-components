f12b9129bb89abd564c2469033a7d81b6ff145af
makan <makan@sevencent.com>
2023-02-03 11:37:01 +0800

    update title

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: 'szdt-admin-components',
+  title: 'szdt',
   mode: 'site',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
